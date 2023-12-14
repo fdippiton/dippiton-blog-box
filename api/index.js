689410d1a33a55bbc8f07e617592abff4a25939c
Francina Dippiton <francinagio@hotmail.com>
2023-12-14 11:57:23 -0400

    Setting login and register

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
+// require("dotenv").config();
 const express = require("express");
-const app = express();
+const config = require("../config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+// Models
+const User = require("./models/User");
 
 // The code snippet provided is using the CORS (Cross-Origin Resource Sharing) middleware for a Node.js application. CORS is a security feature implemented by web browsers to prevent web pages from making requests to a different domain than the one that served the web page.
 
@@ -17,10 +24,57 @@ const cors = require("cors");
 
 // app.use(cors(corsOptions));
 
-app.use(cors());
+const app = express();
+const secretKey = config.SECRET_KEY;
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
 
-app.post("/register", (req, res) => {
-  res.json("test ok");
+// Connect database
+const connectionString = config.database.CONNECTIONSTRING;
+mongoose.connect(connectionString);
+
+app.post("/register", async (req, res) => {
+  // destructure request body for username and password
+  const { username, password } = req.body;
+
+  // Hashear la contraseña antes de almacenarla
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    // create new user  with username and password
+    const userDoc = await User.create({ username, password: hashedPassword });
+    // respond with new user  as JSON
+    res.json(userDoc);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear el usuario." });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  // destructure request body for username and password
+  const { username, password } = req.body;
+
+  const userDoc = await User.findOne({ username });
+
+  // Si el usuario no existe, responde con un error
+  if (!userDoc) {
+    return res.status(401).json({ error: "Credenciales inválidas." });
+  } else {
+    // Compara la contraseña proporcionada con la almacenada en la base de datos
+    const passwordMatch = await bcrypt.compare(password, userDoc.password);
+
+    // Si las contraseñas no coinciden, responde con un error
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Credenciales inválidas." });
+    } else {
+      // res.json(userDoc);
+      jwt.sign({ username, id: userDoc._id }, secretKey, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json("ok");
+      });
+    }
+  }
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useState } from "react";
 
+/*
+This code is a functional component that serves as the register page for a website. The component maintains two pieces of state, username and password, using the useState hook.
+The register function sends a POST request to the server with the entered username and password. The fetch API is used to send the request.
+*/
 function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   async function register(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/register", {
+
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
+
+    if (response.status === 200) {
+      alert("Registration successful.");
+    } else {
+      alert("Registration failed. Try again.");
+    }
   }
 
   return (
